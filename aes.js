da0ed9a2bc3d69e24ee76ca11b39b5edf2397f14
kl313 <60099988+kl313@users.noreply.github.com>
2022-05-28 16:02:23 +0800

    fir

---
+++ aes.js
@@ -0,0 +1,54 @@
+<!doctype html>
+<html> 
+	<head> 
+		<meta charset="UTF-8"> 
+        <link rel="shortcut icon" href="https://support.microsoft.com/socimages/appicons/onedrive.64x64x32.png">
+		<title>ym welcome</title> 
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="apple-touch-icon" href="http://27699465.s21i.faiusr.com/4/ABUIABAEGAAgnqHXiAYovtuf1AUwrQI4rAI.png"/>
+  <meta name="apple-mobile-web-app-title" content="y.m."> 
+	</head> 
+	<body> 
+		<h1 class="titlemain"> Welcome to Y.M. </h1> 
+      	<h2 class="titlesecond">designed by BISHENG &reg;</h2>
+		<div> 
+			<p><code>CODE</code></p> 
+			<p>1223</p>
+        </div>
+         
+         <div>
+        <form action="welcome.php" method="post">
+        uname:<input class="frame" type="text" name="yuan" placeholder="YUAN">
+        <br>
+        content:<input class="frame" type="text" name="meng" placeholder="MENG">
+        <br>
+       <input class="button" type="submit" value="提交">
+        
+        </form>        
+        </div>
+        
+        <div>
+			<table> 
+				<caption>表~1</caption> 
+					<tr> 
+						<th >hea1d</th> 
+                 		 <th >heaid</th> 
+                 		 <th >hsd</th> 
+                  
+					</tr> 
+					<tr> 
+						<td>hso</td> 
+						<td>lala</td> 
+						<td>gbb</td> 
+					</tr> 
+					<tr> 
+						<td>hs1o</td> 
+						<td>lala1</td> 
+						<td>gb1b</td> 
+					</tr> 
+				 
+			</table> 
+		</div>
+       <div>
+       <p> 
+       <div class="leftw"> These are Y.M
